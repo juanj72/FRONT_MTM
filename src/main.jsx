719afce66674a45548da81763944cfac0d71e995
juan jose <juan.jara@campusucc.edu.co>
2023-05-27 16:31:25 -0500

    agregación primera tabla de pacientes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@
 import ReactDom from "react-dom/client";
 import 'semantic-ui-css/semantic.min.css';
 import './scss/global.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import {App} from './App';
 
--- src/pages/Admin/pacientes/pacientes.jsx
+++ src/pages/Admin/pacientes/pacientes.jsx
@@ -1,9 +1,12 @@
-
-
+import {DataPaciente} from '../../../components/Admin/pacientes'
 export const pacientes=()=>{
     return(
         <>
         <h1>Pacientes</h1>
+        
+        <DataPaciente/>
+        
+       
         </>
     )
 }
