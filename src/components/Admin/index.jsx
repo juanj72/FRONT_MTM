5ce7d92bce6a00db6e542b17ee5b79b967175203
juan jose <juan.jara@campusucc.edu.co>
2023-05-28 12:02:16 -0500

    agregacion formulario en modal, sin conexion a api aun

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -2,3 +2,4 @@ export * from './LoginForm';
 export * from './TopMenu';
 export * from './BarraLateral';
 export * from './pacientes';
+export * from './padrinos';
--- src/pages/Admin/padrinos/padrinos.jsx
+++ src/pages/Admin/padrinos/padrinos.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {getToken} from '../../../api/token'
 import * as BScons from 'react-icons/bs';
 import * as Aicons from 'react-icons/ai'
+import {AddPadrino} from '../../../components/Admin/padrinos'
 
 export const padrinos =()=>{
     
@@ -22,7 +23,35 @@ export const padrinos =()=>{
     return(
         <>
         <h1>Padrinos</h1>
-        <button className='btn btn-success' > <BScons.BsPersonFillAdd/> Añadir</button>
+
+
+
+     
+
+<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Agregar padrino</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+      <AddPadrino/>
+
+
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+        <button className='btn btn-success' data-bs-toggle="modal" data-bs-target="#exampleModal" > <BScons.BsPersonFillAdd/> Añadir</button>
         <table className="table  table-hover">
             <thead>
               <tr>
