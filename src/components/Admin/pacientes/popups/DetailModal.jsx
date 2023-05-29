afd263afd1e4df4b5c59b89dbfd336c9cce48c89
juan jose <juan.jara@campusucc.edu.co>
2023-05-28 23:36:23 -0500

    agregacion de modal en pacientes, pendiente por update en ese modulo

--- src/components/Admin/pacientes/popups/DetailModal.jsx
+++ src/components/Admin/pacientes/popups/DetailModal.jsx
@@ -1,3 +1,5 @@
+import * as Facons from 'react-icons/fa';
+import * as Aicons from 'react-icons/ai'
 
 
 export const DetailModal=({paciente})=>{
@@ -11,9 +13,15 @@ export const DetailModal=({paciente})=>{
 
     return(
         <>
-        <h1>Detalle Modal</h1>
-        {data.nombre}
-        {data.apellido}
+        <h1><Facons.FaUserAlt/> {data.nombre} </h1>
+
+        <div className='alert alert-secondary' > <h2><b>Código:</b> {data.id}</h2>  </div>
+        <div className='alert alert-secondary' > <h2><b>Apellido:</b> {data.apellido}</h2>  </div>
+        <div className='alert alert-secondary' > <h2><b>Nui:</b> {data.nui}</h2>  </div>
+        <div className='alert alert-secondary' > <h2><b>Fecha de tratamiento:</b> {data.fecha_inicio_tratamiento}</h2>  </div>
+        <div className='alert alert-secondary' > <h2><b>Fecha de ingreso:</b> {data.fecha_ingreso}</h2>  </div>
+        <div className='alert alert-secondary' > <h2><b>Seguro funebre:</b> {data.seguro_funebre}</h2>  </div>
+        <div className='alert alert-secondary' > <h2><b>Correo:</b> {data.correo}</h2>  </div>
      
 
         
