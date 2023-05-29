fd90e07c41b93563b28c9bb86ce2220ab88a43b4
ReyGabriela <gabrielareyrincon@gmail.com>
2023-05-29 17:39:43 -0500

    Update Navbar.jsx

--- src/components/Client/Navbar/Navbar.jsx
+++ src/components/Client/Navbar/Navbar.jsx
@@ -6,9 +6,7 @@ const Navbar = () => {
     <header>
       <nav className='navbar'>
         <div className='navbar-left'>
-          <a href='/' className='nav-link' id='nav1'>Inicio</a>
-          <a href='/pacientes' className='nav-link' id='nav2'>Pacientes</a>
-          <a href='/padrinos' className='nav-link' id='nav3'>Padrinos</a>
+          <a className='nav-link' id='nav1'>Fundación mujeres trabajando por el Meta</a>
         </div>
       </nav>
       <button className='login-button'>
