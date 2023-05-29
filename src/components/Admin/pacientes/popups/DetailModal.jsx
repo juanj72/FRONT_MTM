3c434cb02cba5141b47563b612d0cb96b3a7bd8d
juan jose <juan.jara@campusucc.edu.co>
2023-05-28 23:49:36 -0500

    agregacion modal, modulo paciente completo, pendiente por programar boton de editar

--- src/components/Admin/pacientes/popups/DetailModal.jsx
+++ src/components/Admin/pacientes/popups/DetailModal.jsx
@@ -2,29 +2,30 @@ import * as Facons from 'react-icons/fa';
 import * as Aicons from 'react-icons/ai'
 
 
-export const DetailModal=({paciente})=>{
-    let data ={}
+export const DetailModal = ({ paciente }) => {
+    let data = {}
 
-   if(paciente!=null){
-    // console.log(paciente)
-    data = paciente
-    console.log(data)
-   }
+    if (paciente != null) {
+        // console.log(paciente)
+        data = paciente
+        console.log(data)
+    }
 
-    return(
+    return (
         <>
-        <h1><Facons.FaUserAlt/> {data.nombre} </h1>
+            <h1><Facons.FaUserAlt /> {data.nombre} </h1>
 
-        <div className='alert alert-secondary' > <h2><b>Código:</b> {data.id}</h2>  </div>
-        <div className='alert alert-secondary' > <h2><b>Apellido:</b> {data.apellido}</h2>  </div>
-        <div className='alert alert-secondary' > <h2><b>Nui:</b> {data.nui}</h2>  </div>
-        <div className='alert alert-secondary' > <h2><b>Fecha de tratamiento:</b> {data.fecha_inicio_tratamiento}</h2>  </div>
-        <div className='alert alert-secondary' > <h2><b>Fecha de ingreso:</b> {data.fecha_ingreso}</h2>  </div>
-        <div className='alert alert-secondary' > <h2><b>Seguro funebre:</b> {data.seguro_funebre}</h2>  </div>
-        <div className='alert alert-secondary' > <h2><b>Correo:</b> {data.correo}</h2>  </div>
-     
+            <div className='alert alert-secondary' > <h2><b>Código:</b> {data.id}</h2>  </div>
+            <div className='alert alert-secondary' > <h2><b>Apellido:</b> {data.apellido}</h2>  </div>
+            <div className='alert alert-secondary' > <h2><b>Nui:</b> {data.nui}</h2>  </div>
+            <div className='alert alert-secondary' > <h2><b>Fecha de tratamiento:</b> {data.fecha_inicio_tratamiento}</h2>  </div>
+            <div className='alert alert-secondary' > <h2><b>Fecha de ingreso:</b> {data.fecha_ingreso}</h2>  </div>
+            <div className='alert alert-secondary' > <h2><b>Seguro funebre:</b> {data.seguro_funebre}</h2>  </div>
+            <div className='alert alert-secondary' > <h2><b>Correo:</b> {data.correo}</h2>  </div>
 
-        
+           
+
+
         </>
     )
 }
