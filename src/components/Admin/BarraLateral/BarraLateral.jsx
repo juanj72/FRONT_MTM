a90e8274c2d74a74df437f0223f47a0340b62103
ReyGabriela <gabrielareyrincon@gmail.com>
2023-05-30 16:43:51 -0500

    Design | Sidebar y topbar

--- src/components/Admin/BarraLateral/BarraLateral.jsx
+++ src/components/Admin/BarraLateral/BarraLateral.jsx
@@ -1,56 +1,49 @@
 import './SideBar.scss';
-import * as FaIcons from 'react-icons/fa';
-import * as FIcons from 'react-icons/fi';
-import * as MDcons from 'react-icons/md';
-import * as BScons from 'react-icons/bs';
-import {NavLink,Link} from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom';
+import { FaHome, FaChild } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { BsFillMoonFill } from 'react-icons/bs';
+import { MdOutlineFamilyRestroom, MdFoundation } from 'react-icons/md';
 
-
-export const BarraLateral =()=>{
-
-  return(
-    <>
-  <header>
-        <div className="sidebar">
-          <div className="sidebar-header">
-            {/* <h3> <MDcons.MdFoundation/> MTM</h3> */}
-            <h2><MDcons.MdFoundation/> MTM</h2>
-          </div>
-          <nav className="sidebar-nav">
-            <ul>
-              <li>
-             
-                <Link to="/admin"> <FaIcons.FaHome/> inicio</Link> 
-              
-               
-              </li>
-              <li>
-               
-                <Link to="/admin/paciente"> <FaIcons.FaChild/>  Pacientes</Link> 
-                {/* <a href=""> <FaIcons.FaChild/> Paciente</a> */}
-              </li>
-              <li>
-                
-                <Link to="/admin/padrinos"><FIcons.FiUsers/> Padrinos</Link> 
-                
-                
-              </li>
-              <li>
-              <Link to="/admin/sueños"><BScons.BsFillMoonFill/> Sueños</Link> 
-               
-              </li>
-              <li>
-              
-                <Link to="/admin/familiares"><MDcons.MdOutlineFamilyRestroom/> Familiares</Link> 
-              </li>
-
-            </ul>
-          </nav>
+export const BarraLateral = () => {
+  return (
+    <header>
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <h2>
+            <MdFoundation />MTM
+          </h2>
         </div>
-
-      </header>
- 
-    </>
-  )
-    
-}
+        <nav className="sidebar-nav">
+          <ul>
+            <li>
+              <Link to="/admin">
+                <FaHome /> Inicio
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/paciente">
+                <FaChild /> Pacientes
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/padrinos">
+                <FiUsers /> Padrinos
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/sueños">
+                <BsFillMoonFill /> Sueños
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/familiares">
+                <MdOutlineFamilyRestroom /> Familiares
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
--- src/components/Admin/TopMenu/TopMenu.jsx
+++ src/components/Admin/TopMenu/TopMenu.jsx
@@ -1,42 +1,41 @@
 import './TopMenu.scss';
-import {useAuth} from '../../../hooks'
-import {NavLink,Link} from 'react-router-dom'
-import {Icon,Menu} from 'semantic-ui-react'
-import * as Fccons from 'react-icons/fc';
-import * as Ricons from 'react-icons/ri';
+import { useAuth } from '../../../hooks';
+import { NavLink, Link } from 'react-router-dom';
+import { Icon, Menu } from 'semantic-ui-react';
+import { RiLogoutBoxLine } from 'react-icons/ri';
+import { FaHome } from 'react-icons/fa';
 
-export const TopMenu=()=>{
-    const {auth,logout} =useAuth();
-    const renderName=()=>{
-        if(auth.me?.first_name && auth.me?.last_name){
-            return `${auth.me.first_name} ${auth.me.last_name}`
-        }else{
-            return auth.me?.email
-        }
+export const TopMenu = () => {
+  const { auth, logout } = useAuth();
+
+  const renderName = () => {
+    if (auth.me?.first_name && auth.me?.last_name) {
+      return `${auth.me.first_name} ${auth.me.last_name}`;
+    } else {
+      return auth.me?.email;
     }
+  };
 
-    const renderDatos =()=>{
-        return `${auth.me.is_staff}`
-    }
-    return(
-        <>
-       <Menu fixed='top' className='top-menu-admin'>
-        <Menu.Item className='top-menu-admin__logo'>
-            <img src="https://i0.wp.com/www.fundacionmtm.com/wp-content/uploads/2020/09/cropped-logocorazon.png?fit=32%2C32&ssl=1" className='logo' alt="" />               
+  return (
+    <Menu fixed="top" className="top-menu-admin">
+      <Menu.Item className="top-menu-admin__logo">
+        <img
+          src="https://i0.wp.com/www.fundacionmtm.com/wp-content/uploads/2020/09/cropped-logocorazon.png?fit=32%2C32&ssl=1"
+          className="logo"
+          alt=""
+        />
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item>Hi, {renderName()}</Menu.Item>
+        <Menu.Item onClick={logout}>
+          <RiLogoutBoxLine className="icon2" />
         </Menu.Item>
-        <Menu.Menu position='right'>
-        <Menu.Item>Hola, {renderName()} </Menu.Item>
-        <Menu.Item onClick={logout} >
-        <Ricons.RiLogoutBoxRLine className='icon2'/>
+        <Menu.Item>
+          <Link to="/">
+            <FaHome />
+          </Link>
         </Menu.Item>
-        <Menu.Item  >
-            
-            <Link to='/'> <Fccons.FcHome/></Link>
-        </Menu.Item>
-        </Menu.Menu>
-        
-       </Menu>
-
-        </>
-    )
-}
+      </Menu.Menu>
+    </Menu>
+  );
+};
